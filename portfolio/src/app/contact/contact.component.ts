d5376e0582e065383f4c68a98def9c3fab75565d
HakasOD <hak.dimozantos@gmail.com>
2025-04-09 11:47:18 +1000

    Add google analytics

--- portfolio/src/app/contact/contact.component.ts
+++ portfolio/src/app/contact/contact.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { IconComponent } from "../shared/socials/icon/icon.component";
 import { SocialsComponent } from "../shared/socials/socials.component";
 
 @Component({
   selector: 'app-contact',
-  imports: [IconComponent, SocialsComponent],
+  imports: [ SocialsComponent],
   templateUrl: './contact.component.html',
 })
 export class ContactComponent {
